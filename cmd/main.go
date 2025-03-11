979767e27806b54ac91ed1b1d104381ffb3da9bb
Parsa <parsadarbouy@gmail.com>
2025-03-10 18:14:54 -0600

    refactor main.go to create Kubernetes configuration file and update values; add val_back.yaml for default settings

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
+	"github.com/parsaDarbouy/Platform/pkg"
 )
-
-const kind_deploymnent = "Deployment"
-const kind_service = "Service"
-const kind_ingress = "Ingress"
 
 func check(e error) {
     if e != nil {
@@ -15,29 +12,23 @@ func check(e error) {
     }
 }
 
-func CreateFile(filename string) {
-	fmt.Println("Creating file")
-	file, err := os.Create(filename)
-
-	check(err)
-
-	fmt.Printf("File with %s name created\n", file.Name())
-}
-
-func FileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
-}
-
-var kind_test string = "Deployment" 
+var val_back string = "val_back" 
+var replicas = 1
+var repository = "docker.io/redis"
+var tag = "latest"
+var pullPolicy = "IfNotPresent"
+var containerPort = 6379
+var serviceType = "ClusterIP"
+var port = 6379
+var targetPort = 6379
 
 func main() {
-	if !FileExists(kind_test + ".yaml") {
-		CreateFile(kind_test + ".yaml")
-	} else {
-		fmt.Println("File already exists")
-	}
+	file, err := os.OpenFile(val_back+".yaml", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	check(err)
+	defer file.Close()
+
+	_,err = file.Write([]byte(pkg.MakeValues(replicas, repository, tag, pullPolicy, containerPort, serviceType, port, targetPort)))
+	check(err)
+
+	fmt.Printf("File with %s name created or overwritten\n", file.Name())
 }
